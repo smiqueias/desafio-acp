a535296a200e8d843717940d3de3aa943700da28
saulo <saulomiqueias7@gmail.com>
2021-06-07 14:08:44 -0300

    crud ready

--- backend/produtos-api/app/Http/Controllers/Api/ProductController.php
+++ backend/produtos-api/app/Http/Controllers/Api/ProductController.php
@@ -44,37 +44,49 @@ class ProductController extends Controller
 
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show(int $id) : JsonResponse
     {
-        //
+        try {
+            $product = $this->product->findOrFail($id);
+            return response()->json([
+                'data' => [
+                    $product
+                ]
+            ],200);
+        } catch (\Exception $exception) {
+            return response()->json($exception->getMessage(),401);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(Request $request, int $id) : JsonResponse
     {
-        //
+        $data = $request->all();
+        try {
+            $product = $this->product->findOrFail($id);
+            $product->update($data);
+            return response()->json([
+                'data' => [
+                    'msg' => 'Produto atualizado com sucesso!'
+                ]
+            ],201);
+        } catch (\Exception $exception) {
+            return response()->json($exception->getMessage(),401);
+        }
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function destroy( int $id) : JsonResponse
     {
-        //
+        try {
+            $product = $this->product->findOrFail($id);
+            $product->delete();
+            return response()->json([
+                'data' => [
+                    'msg' => 'Produto deletado com sucesso!'
+                ]
+            ],200);
+        } catch (\Exception $exception) {
+            return response()->json($exception->getMessage(),401);
+        }
     }
 }
